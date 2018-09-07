37968dfd05d628694f3c52a23a15641aa8eee5ff
matthewkirby <matthewkirby@email.arizona.edu>
2018-09-07 11:08:34 -0700

    Added a more extensive jackknife test

--- kirbytoolkit/tests/test_jackknife.py
+++ kirbytoolkit/tests/test_jackknife.py
@@ -7,3 +7,10 @@ def test_jackknife_arr():
     jkvar_true = 0.26
     jkvar_code = ktk.jackknife_array(arr)
     np.testing.assert_almost_equal(jkvar_code, jkvar_true, decimal=7)
+
+    # Test a longer array
+    arr = np.loadtxt('randomdata.dat')
+    jkvar_true = 0.003031329328040*0.003031329328040
+    jkvar_code = ktk.jackknife_array(arr)
+    np.testing.assert_almost_equal(jkvar_code, jkvar_true, decimal=7)
+    
